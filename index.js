8e7e0435f06af983d17243d79b79f8810ad46a5c
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-02 23:35:15 -0800

    90 degrees works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function processImage (img) {
   // img.height, .width, .data [r,g,b,a,r,g,b,a...]
-  flipVertical(img);
+  rotate90(img);
 }
 
 function copy (img) {
@@ -68,9 +68,41 @@ function flipVertical (img) {
   return result;
 }
 
-function flipHorizontal (img) {}
+function flipHorizontal (img) {
+  var result = img;
+  var data = img.data;
+  var newData = data.join('').split('');
+  var step = img.width * 4;
+  for(var i = 0; i < img.data.length; i += step) {
+    for(var j = 0; j < step; j += 4) {
+      newData[i + step - j - 4] = data[i + j];
+      newData[i + step - j - 3] = data[i + j + 1];
+      newData[i + step - j - 2] = data[i + j + 2];
+      newData[i + step - j - 1] = data[i + j + 3];
+    }
+  }
+  result.data = newData;
+  return result;
+}
 
-function rotate90 (img) {}
+function rotate90 (img) {
+  var result = img;
+  var data = img.data;
+  var newData = data.join('').split('');
+  var step = img.width * 4;
+  var index = 0;
+  for(var i = img.data.length - step; i < img.data.length; i += 4) {
+    for(var j = i; j > 0; j -= step) {
+      newData[index] = data[j];
+      newData[index + 1] = data[j + 1];
+      newData[index + 2] = data[j + 2];
+      newData[index + 3] = data[j + 3];
+      index += 4;
+    }
+  }
+  result.data = newData;
+  return result;
+}
 
 function blur (img, blurFactor) {}
 
